79384e16134815f8afc8820f3b6d521954fb77fb
Antonio Maestre Vargas <antoniomaestrevargasgmail.com@MacBook-Pro-de-Antonio.local>
2021-10-12 19:03:49 +0200

    pokemon

---
+++ fetch.js
@@ -0,0 +1,51 @@
+const lista=document.querySelector('.lista');
+
+
+
+const getPokemon=async() => {
+    const trokemon=[];
+    for (let i = 1; i < 151; i++) {
+        const result = await fetch('https://pokeapi.co/api/v2/pokemon/'+i);
+        const pokemonData = await result.json();
+        trokemon.push(pokemonData);
+        console.log(trokemon);
+        
+    }
+    
+    
+    const pokemons=trokemon.map((pokemon)=>({
+        name:pokemon.name,
+        image:pokemon.sprites.other['official-artwork']['front_default'],
+        id:pokemon.id
+
+    }))
+
+    pintarPokemon(pokemons)
+}
+const pintarPokemon=(pokemons)=>{
+   const pokemonsHtml=pokemons.map(
+        (element)=>
+        `<li><h2>${element.name}</h2>
+        <img src="${element.image}"
+        /><p>${element.id}</p></li>`).join(' ');
+        lista.innerHTML=pokemonsHtml
+    
+
+
+
+}
+
+getPokemon();
+    
+  //  pintarPokemon(pokemons)
+//}
+//const pintarPokemon=pokemons=>{
+  //  const pokemonsHtml=pokemons.map(
+    //    (element)=>
+     //   `<li><h2>${element.name}</h2>
+      //  <img src="${element.image}"/></li>`).join(' ');
+     //   lista.innerHTML=pokemonsHtml
+    
+//}
+
+
